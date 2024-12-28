7c260c4807b74ffd54c75f227d8e47d28cbffda4
Peihao <li.peihao.62s@st.kyoto-u.ac.jp>
2024-12-29 01:08:20 +0900

    Changing license to LGPL

--- include/EndemicOT/Endemic_OT_C.h
+++ include/EndemicOT/Endemic_OT_C.h
@@ -1,19 +1,19 @@
 /*
 This file is part of EOTKyber of Abe-Tibouchi Laboratory, Kyoto University
-Copyright (C) 2023-2024  Kyoto University
-Copyright (C) 2023-2024  Peihao Li <li.peihao.62s@st.kyoto-u.ac.jp>
+Copyright © 2023-2024  Kyoto University
+Copyright © 2023-2024  Peihao Li <li.peihao.62s@st.kyoto-u.ac.jp>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
+EOTKyber is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+EOTKyber is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
+You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 *******************************************************************************
